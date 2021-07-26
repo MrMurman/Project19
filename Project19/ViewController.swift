fdf1b4fa117f06453208665031d1c0de7b62d41e
MrMurman <themanofswift.gmail.com>
2021-07-26 20:20:45 +0300

    Initial Commit

---
+++ Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Андрей Бородкин on 26.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
